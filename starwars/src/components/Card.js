1e3439c672ced28b546297549b08aee5f0a069f0
Pherpher089 <Christutor089@gmail.com>
2019-05-17 19:56:18 -0700

    Finished the MVP. Styling is minimal

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -4,7 +4,24 @@ import './StarWars.css';
 const Card = props => {
     return(
         <div className="card">
-            <h1>{props.name}</h1>
+            <div className='card-header'>
+                <h1>{props.name}</h1>
+            </div>
+            <div>
+                <div className='card-basic-info'>
+                    <div>
+                        <p>DOB: {props.birthYear}</p>
+                        <p>Gender: {props.gender}</p>
+                        <p>Heght: {props.height}</p>
+                        <p>Mass: {props.mass}</p>
+                    </div>
+                    <div>
+                        <p>Eye Color: {props.eyeColor}</p>
+                        <p>Hair Color: {props.hairColor}</p>
+                        <p>Skin Color: {props.skinColor}</p>
+                    </div>           
+                </div>
+            </div>
         </div>
     )
 }
--- starwars/src/components/CardList.js
+++ starwars/src/components/CardList.js
@@ -4,21 +4,21 @@ import Card from './Card.js'
 
 const CardList = props =>{
     return(
-        <div>
+        <div className='card-list'>
         {props.starwarsChars.map(card =>(
             <Card 
-                birth-year={card.birth_year}
+                birthYear={card.birth_year}
                 created={card.created}
                 edited={card.edited}
-                eye-color={card.eye_color}
+                eyeColor={card.eye_color}
                 films={card.films}
-                gender={card.male}
-                hair-color={card.hair_color}
+                gender={card.gender}
+                hairColor={card.hair_color}
                 height={card.height}
                 homeworld={card.homeworld}
                 mass={card.mass}
                 name={card.name}
-                skin-color={card.skin_color}
+                skinColor={card.skin_color}
                 species={card.species}
                 starships={card.starships}
                 url={card.url}
